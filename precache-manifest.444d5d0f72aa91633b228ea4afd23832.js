1c859969f483402936472f52096ce6a0ee436f15
Rahim Ansari <rahimansari4560@gmail.com>
2022-07-30 14:19:44 +0530

    Updates

---
+++ precache-manifest.444d5d0f72aa91633b228ea4afd23832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897e1e6ec5f525ef78a2eeb28adcb6e6",
+    "url": "/reduxmyapp/index.html"
+  },
+  {
+    "revision": "471b4c00ccd945cf760b",
+    "url": "/reduxmyapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2d5f5e3571a1ff834728",
+    "url": "/reduxmyapp/static/js/2.a27fd9bf.chunk.js"
+  },
+  {
+    "revision": "471b4c00ccd945cf760b",
+    "url": "/reduxmyapp/static/js/main.17216acb.chunk.js"
+  },
+  {
+    "revision": "43c37d71535538a593d8",
+    "url": "/reduxmyapp/static/js/runtime~main.e30f2e50.js"
+  }
+]);
